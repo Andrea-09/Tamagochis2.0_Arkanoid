0813867ad30109310932c333c2dc84526a96b975
javierbeltran425 <00019315@uca.edu.sv>
2020-06-02 17:55:16 -0600

    Fixes

--- Proyecto Arkanoid/Proyecto Arkanoid/Playing.Designer.cs
+++ Proyecto Arkanoid/Proyecto Arkanoid/Playing.Designer.cs
@@ -31,13 +31,23 @@ namespace Proyecto_Arkanoid
         /// </summary>
         private void InitializeComponent()
         {
+            this.game1 = new Proyecto_Arkanoid.Game();
             this.SuspendLayout();
+            // 
+            // game1
+            // 
+            this.game1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.game1.Location = new System.Drawing.Point(0, 0);
+            this.game1.Name = "game1";
+            this.game1.Size = new System.Drawing.Size(933, 519);
+            this.game1.TabIndex = 0;
             // 
             // Playing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(933, 519);
+            this.Controls.Add(this.game1);
             this.Name = "Playing";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Playing";
@@ -45,5 +55,7 @@ namespace Proyecto_Arkanoid
         }
 
         #endregion
+
+        private Proyecto_Arkanoid.Game game1;
     }
 }
